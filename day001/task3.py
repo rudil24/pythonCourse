fd2f21062032be9c13542ebfa23b46ab588a0395
Rudi Lewis <rudil24@gmail.com>
2026-01-01 09:25:01 -0700

    Day 1 band generator exercise

---
+++ day001/task3.py
@@ -0,0 +1,7 @@
+# print("Hello" + " " + "Angela")
+# commented above line and replaced with input function
+# print ("Hello" + " " + input("What is your name?") + "!")
+username = input("What is your name?")
+# print("Your name has " + str(len(name)) + " characters.")
+length = len(username)
+print(length)
